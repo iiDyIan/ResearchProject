4057e30545f7aab860ea00300c3fc725826a26aa
Dylan <dylan@iistudios.org>
2022-02-23 11:44:17 -0500

    added core loop

--- NestedLoops.py
+++ NestedLoops.py
@@ -28,3 +28,9 @@ inputVariableIndexes = {
   energyInFrequencyInterval = variables["EnergyInFrequency"][:],
   relEnergyInFrequency = variables["RelativeEnergyInFrequency"][:]
 }
+
+for s in range(len(skewness)) and for k in range(len(kurtosis)) and for pwP in range(len(peakWavePeriod)) and for pwL in range(len(peakWaveLength)) and for st in range(len(steepness)) and for b in range(len(bandwith)) and for bfi in range(len(bejaminFeirIndex)) and for ctc in range(len(crestTroughCorrelation)) and for eiFI in range(len(energyInFrequencyInterval)) and for reFI in range(len(relEnergyInFrequency)):
+  
+  
+  
+  
